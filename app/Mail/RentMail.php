e53ba91eb78a077ac400dca2d4bfc2a2a4a3d68f
rickyhussein <94778902+rickyhussein@users.noreply.github.com>
2026-01-17 22:47:20 +0700

    Update RentMail.php

--- app/Mail/RentMail.php
+++ app/Mail/RentMail.php
@@ -29,6 +29,6 @@ class RentMail extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        return $this->markdown('rent.mail')->subject('Rent SmartKost');
+        return $this->markdown('rents.mail')->subject('Rent SmartKost');
     }
 }
