127366693fcac31aa70b7005ab6a3ec0c0010ecd
rickyhussein <94778902+rickyhussein@users.noreply.github.com>
2026-01-22 00:15:51 +0700

    update

---
+++ app/Console/Commands/UpdateStatusProperty.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PropertyRoom;
+use App\Models\UserProperty;
+use Illuminate\Console\Command;
+
+class UpdateStatusProperty extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:updatestatusproperty';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Automatic Update Status';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $up = UserProperty::find(5);
+        $up->update([
+            'is_active' => null,
+            'status' => 'Non Aktif',
+        ]);
+
+        $room = PropertyRoom::find($up->id);
+        $room->update([
+            'is_available' => null,
+        ]);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('command:updatestatusproperty')->dailyAt('00:17');
     }
 
     /**
