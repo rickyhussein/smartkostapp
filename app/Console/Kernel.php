b9e505edf8733ff018a65217fb972842d70f8f7b
rickyhussein <94778902+rickyhussein@users.noreply.github.com>
2026-01-22 00:19:16 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:updatestatusproperty')->dailyAt('00:17');
+        $schedule->command('command:updatestatusproperty')->dailyAt('00:20');
     }
 
     /**
